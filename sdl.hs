f8e9370e0f75e2697bfa5dbbc6dbb0069413e0f8
Antti Keränen <detegr@gmail.com>
2014-03-12 15:38:09 +0200

    Cleaned up code + started to use lenses

--- sdl.hs
+++ sdl.hs
@@ -1,41 +1,39 @@
-import Graphics.UI.SDL.Image
+{-# LANGUAGE TemplateHaskell #-}
+
+import Control.Monad.Trans
+import Control.Monad.Trans.State
+import Control.Lens
+import Data.Default
 import Graphics.UI.SDL
+import Graphics.UI.SDL.Image
 import Control.Monad
+import Control.Applicative ((<$>),(<*>))
 
-data Player = Player
-            {
-                x :: Int,
-                y :: Int
-            }
-zeroPlr :: Player
-zeroPlr = Player 0 0
+data Player = Player { _plrX :: Int
+                     , _plrY :: Int }
+makeLenses ''Player
 
-data GameState = GameState
-               {
-                   player     :: Player,
-                   keys       :: KeyState,
-                   notRunning :: Bool
-               }
-mkSt :: Player -> KeyState -> GameState
-mkSt player keystate = GameState player keystate False
+instance Default Player where
+  def = Player 0 0
 
-data KeyState = KeyState
-              {
-                   keyUp    :: Bool,
-                   keyDown  :: Bool,
-                   keyLeft  :: Bool,
-                   keyRight :: Bool
-              }
-zeroKeys :: KeyState
-zeroKeys = KeyState False False False False
+data KeyState = KeyState { _keyUp        :: Bool
+                         , _keyDown      :: Bool
+                         , _keyLeft      :: Bool
+                         , _keyRight     :: Bool
+                         , _keyUnhandled :: Bool}
+makeLenses ''KeyState
+instance Default KeyState where
+  def = KeyState False False False False False
 
-data GameData = GameData
-              {
-                  screen :: Surface,
-                  sprite :: Surface
-              }
-zeroState :: GameState
-zeroState = GameState zeroPlr zeroKeys False
+data GameData = GameData { _screen :: Surface
+                         , _sprite :: Surface }
+makeLenses ''GameData
+
+data GameState = GameState { _player   :: Player
+                           , _keys     :: KeyState
+                           , _running  :: Bool
+                           , _game     :: GameData}
+makeLenses ''GameState
 
 loadImage :: String -> IO Surface
 loadImage f = load f >>= displayFormat
@@ -44,49 +42,56 @@ applySurface :: Int -> Int -> Surface -> Surface -> IO Bool
 applySurface x y src dst = blitSurface src Nothing dst offset
    where offset = Just (Rect x y 0 0)
 
-loop :: GameData -> GameState -> IO ()
-loop g s = do state <- pollEvents s
-              let (Player x y) = player s
-              applySurface x y (sprite g) (screen g)
-              Graphics.UI.SDL.flip (screen g)
-              unless (notRunning state) (loop g state)
+loop :: StateT GameState IO ()
+loop = get >>= \st -> do
+  pollEvents
+  movePlayerM
+  liftIO $ do
+    _ <- applySurface (st ^. player.plrX)
+                      (st ^. player.plrY)
+                      (st ^. game.sprite)
+                      (st ^. game.screen)
+    Graphics.UI.SDL.flip (st ^. game.screen)
+  when (st ^. running) loop
 
-pollEvents :: GameState -> IO GameState
-pollEvents g = pollEvent >>= \e -> 
-               case e of Quit      -> return $ (GameState (player g) zeroKeys True)
-                         KeyDown k -> return $ setKey k True g
-                         KeyUp   k -> return $ setKey k False g
-                         _         -> return $ tick g
 
-setKey :: Keysym -> Bool -> GameState -> GameState
-setKey (Keysym k _ _) b g = case k of SDLK_UP    -> mkSt (player g) (KeyState b d l r)
-                                      SDLK_DOWN  -> mkSt (player g) (KeyState u b l r)
-                                      SDLK_LEFT  -> mkSt (player g) (KeyState u d b r)
-                                      SDLK_RIGHT -> mkSt (player g) (KeyState u d l b)
-                                      _          -> g
-                          where (KeyState u d l r) = keys g
+pollEvents :: StateT GameState IO ()
+pollEvents = liftIO pollEvent >>= \e -> get >>= \st ->
+             case e of
+                Quit      -> put $ running  .~ False $ st
+                KeyDown k -> put $ getKey k .~ True  $ st
+                KeyUp   k -> put $ getKey k .~ False $ st
+                _         -> return ()
 
-movePlayer :: KeyState -> Player -> Player
-movePlayer (KeyState u d l r) (Player x y)
-    | u && l = Player (x-1) (y-1)
-    | u && r = Player (x+1) (y-1)
-    | u      = Player x (y-1)
-    | d && l = Player (x-1) (y+1)
-    | d && r = Player (x+1) (y+1)
-    | d      = Player x (y+1)
-    | l      = Player (x-1) y
-    | r      = Player (x+1) y
-    | otherwise = Player x y
+getKey :: Functor f => Keysym -> (Bool -> f Bool) -> GameState -> f GameState
+getKey (Keysym k _ _) =
+  case k of
+    SDLK_UP    -> keys . keyUp
+    SDLK_DOWN  -> keys . keyDown
+    SDLK_LEFT  -> keys . keyLeft
+    SDLK_RIGHT -> keys . keyRight
+    _          -> keys . keyUnhandled
 
-tick :: GameState -> GameState
-tick g = GameState (movePlayer (keys g) (player g)) (keys g) False
+fromBool :: Bool -> Int
+fromBool b = if b then 1 else 0
 
-initData :: IO GameData
-initData = do screen <- setVideoMode 800 600 32 [SWSurface]
-              setCaption "test" []
-              image <- loadImage "img/dude.png"
-              return $ GameData screen image
+movePlayer :: GameState -> GameState
+movePlayer g = player . plrX +~ fromBool (ks ^. keyRight) $
+               player . plrX -~ fromBool (ks ^. keyLeft) $
+               player . plrY +~ fromBool (ks ^. keyDown) $
+               player . plrY -~ fromBool (ks ^. keyUp) $ g
+  where ks = g ^. keys
 
-main = withInit [InitEverything] $ do
-       gdata <- initData
-       loop gdata zeroState
+movePlayerM :: StateT GameState IO ()
+movePlayerM = get >>= put . movePlayer
+
+initState :: IO GameState
+initState = do
+  setCaption "test" []
+  gdata <- GameData <$>
+             setVideoMode 800 600 32 [SWSurface] <*>
+             loadImage "img/dude.png"
+  return $ GameState def def True gdata
+
+main :: IO()
+main = withInit [InitEverything] $ initState >>= void . evalStateT loop
